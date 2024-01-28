1ff638581997f7b221ef10f26c9e2d15f2d3856a
Brandon Ding <dexon888@gmail.com>
2024-01-27 22:33:25 -0500

    Testing url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext"
 import Routes from "./routes"
 
 function App() {
-  const baseURL = process.env.REACT_APP_API_BASE_URL;
+  const baseURL = process.env.REACT_APP_API_BASE_URL || 'https://fallback-url.com';
   axios.defaults.baseURL = baseURL;
   console.log(baseURL)
   axios.defaults.withCredentials = true
